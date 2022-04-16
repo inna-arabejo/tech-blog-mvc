8d7860ccf1c0e1d4c1114d3803fd4b1f13998466
Anne Saber <iarabejo3@gmail.com>
2022-04-15 23:55:02 -0500

    Created dashboard route.

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -13,59 +13,34 @@ router.get('/', withAuth, (req, res) => {
     res.render('all-posts', { 
       layout: 'dashboard', 
       posts
-    });
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
+    }).catch (err => {
     res.status(500).json(err);
-  }
+  })
 });
 
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
+router.get('/new', withAuth, (req, res) => {
+  res.sender('new-post', {
+    layout: 'dashboard'
+  })
+});
 
-    const user = userData.get({ plain: true });
+router.get('/edit:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id)
+  .then(postData => {
+    if (postData) {
+      const post = postData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
+      res.render('edit-post', {
+        layout: 'dashboard', 
+        post
+      });
+    } else {
+      res.status(404).end();
+    }
+  }).catch (err => {
     res.status(500).json(err);
-  }
+  })
 });
 
-router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
-
-  res.render('login');
-});
 
 module.exports = router;
