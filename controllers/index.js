23bb3dec2340d182655f58ad304653ca71d9ba58
Anne Saber <iarabejo3@gmail.com>
2022-04-16 00:44:22 -0500

    Routed api, dashboard, and html route in index.js file.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const htmlRoutes = require('./homeRoutes');
+const htmlRoutes = require('./htmlRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
 router.use('/', htmlRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
