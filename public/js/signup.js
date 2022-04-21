b483d97e412be340c52ef90c84535165dda48451
Anne Saber <iarabejo3@gmail.com>
2022-04-21 18:24:44 -0500

    Send post request api endpoint of signup.

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,47 +1,28 @@
-const newFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const usernameElement = document.querySelector('#username-input-signup').value.trim();
+  const passwordElement = document.querySelector('#password-input-signup').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (username && password ) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      body: JSON.stringify({ 
+        username: usernameElement.value, 
+        password: passwordElement.value }),
+      headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      // If successful, redirect the browser to the dashboard
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to create project');
-    }
-  }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
+      alert(response.statusText);
     }
   }
 };
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
