cc3450663834bf0d069664a9f61bc4e0414b2e56
Anne Saber <iarabejo3@gmail.com>
2022-04-21 17:41:31 -0500

    Give user ability to comment, delete, edit in public js folder.

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,8 +1,8 @@
 const commentFormHandler = async (event) => {
   event.preventDefault();
 
-  const postId = document.querySelector('input[name="post-id"]').value;
-  const body = document.querySelector('textarea[name="comment-body"]').value;
+  const postId = document.querySelector('input[name="post-id"]').value.trim();
+  const body = document.querySelector('textarea[name="comment-body"]').value.trim();
 
   if (body) {
     await fetch(`/api/comment`, {
--- public/js/edit.js
+++ public/js/edit.js
@@ -1,47 +1,26 @@
-const newFormHandler = async (event) => {
+const  editFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const titleElement = document.getElementById('post-title').value.trim();
+  const bodyElement = document.getElementById('post-body').value.trim();
+  const postId = document.getElementById('post-id').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
-      method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+fetch(`/api/post/` + postId.value, {
+    method: 'PUT',
+    body: JSON.stringify({ title: titleElement.value,
+    body: bodyElement.value}),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create project');
-    }
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert('Failed to edit post.');
   }
-};
-
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to delete project');
-    }
-  }
-};
+}
 
 document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
-
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector('.edit-post-form')
+  .addEventListener('submit', editFormHandler);
