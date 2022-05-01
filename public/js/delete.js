050542048b5a55d48218ae7ff506cf76abfd32b9
Anne Saber <iarabejo3@gmail.com>
2022-05-01 15:54:46 -0500

    Fixed the ability to delete a post in delete.js in public folder.

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,11 +1,18 @@
-const deletePostHandler = async (event) => {
-  console.log('clicked', event)
+const deleteFormHandler = async (event) => {
   event.preventDefault();
 
-  const postId = document.getElementById('post-id').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
 
-    fetch(`/api/post` + postId.value, {
+    const response = await fetch(`/api/posts/${id}`, {
       method: 'DELETE',
+      body: JSON.stringify({
+        post_id: id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
     });
 
     if (response.ok) {
@@ -16,5 +23,5 @@ const deletePostHandler = async (event) => {
 };
 
 document
-  .querySelector('.delete-button')
-  .addEventListener('click', deletePostHandler);
+  .querySelector('.delete-post-btn')
+  .addEventListener('click', deleteFormHandler);
