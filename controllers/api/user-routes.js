b16cc654b6957a3e08f0f2fe0054dcbfb79e9faa
Anne Saber <iarabejo3@gmail.com>
2022-05-01 14:29:51 -0500

    Finds all and single user routes in user-routes.js file.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,19 +2,76 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 
+router.get('/', (req, res) => {
+  User.findAll({
+    attributes: { 
+      exclude: ['[password'] 
+    }
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
+});
+
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { 
+      exclude: ['password'] 
+    },
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Post,
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at'
+      ]
+    },
+    {
+      model: Comment,
+      attributes: ['id', 'comment_text', 'created_at'],
+      include: {
+          model: Post,
+          attributes: ['title']
+      }
+    },
+    {
+      model: Post,
+      attributes: ['title'],
+    }
+    ]
+  })
+  .then(dbUserData => {
+    if (!dbUserData) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(dbUserData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
 router.post('/', (req, res) => {
   User.create({
-      username: req.body.username,
-      password: req.body.password,
-    })
-    .then(userData => {
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.username = userData.username;
-        req.session.loggedIn = true;
+    username: req.body.username,
+    password: req.body.password,
+  })
+  .then(userData => {
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.username = userData.username;
+      req.session.loggedIn = true;
 
-        res.status(200).json(userData);
-      });
+      res.status(200).json(userData);
+    });
   })
   .catch (err => {
     console.log(err);
@@ -25,11 +82,11 @@ router.post('/', (req, res) => {
 
 router.post('/login', (req, res) => {
   User.findOne({
-      where: {
-        username: req.body.username,
-      },
-    })
-    .then(userData => {
+    where: {
+      username: req.body.username,
+    },
+  })
+  .then(userData => {
     if (!userData) {
       res.status(400).json({ message: 'Incorrect username or password. Please try again!' });
       return;
@@ -63,6 +120,27 @@ router.post('/logout', (req, res) => {
   } else {
     res.status(404).end();
   }
+});
+
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(userData => {
+    if (!userData[0]) {
+      res.status(404).json({ message: 'No user found with this id' });
+      return;
+    }
+    res.json(userData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 router.delete('/user/:id', (req, res) => {
