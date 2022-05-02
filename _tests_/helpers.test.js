fe548088955cba7224840079f310289b328d3bd0
Anne Saber <iarabejo3@gmail.com>
2022-05-01 19:59:38 -0500

    Added helpers to format the date.

--- _tests_/helpers.test.js
+++ _tests_/helpers.test.js
@@ -1,5 +1,5 @@
 const { format_date } = require('../utils/helpers');
 test('format_date() returns a date string', () => {
-    const date = new Date('2020-03-20 16:12:03');
-    expect(format_date(date)).toBe('3/20/2020');
+    const date = new Date('2022-05-01 19:55:03');
+    expect(format_date(date)).toBe('5/01/2022');
 });
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
 const app = express();
-const PORT = process.env.PORT || 3001;
-
+const PORT = process.env.PORT || 3000;
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: 'Super secret secret',
-  cookie: {
-    maxAge: 3600,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'strict',
-  },
+  cookie: { maxAge: 36000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,21 +19,13 @@ const sess = {
   }),
 };
 
-const hbs = exphbs.create({ 
-  helpers: {
-    format_date: date => {
-      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    }
-  }
-});
-  
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
