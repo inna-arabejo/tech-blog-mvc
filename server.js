2be7210febe5ac9c5dc47bab5beb5fbada97a516
Anne Saber <iarabejo3@gmail.com>
2022-05-01 19:04:29 -0500

    Updated server.js file with proper connections.

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -25,22 +23,21 @@ const sess = {
   }),
 };
 
-app.use(session(sess));
-
 const hbs = exphbs.create({ 
   helpers: {
-  format_date: date => {
-    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    format_date: date => {
+      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+    }
   }
-} });
-
+});
+  
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
